0ef15eef85ab499f48ae2904d7653c9991930cd6
Haddle <94085928+hadll@users.noreply.github.com>
2024-10-22 23:31:00 +1100

    bookmarks save

--- inject-button.js
+++ inject-button.js
@@ -35,76 +35,81 @@ function saveData(newBookmark) {
 }
 
 async function getData(api_url) {
-    chrome.runtime.sendMessage(
-        { type: "FETCH_DATA", url: api_url },
-        (response) => {
-            if (response.success) {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(response.data.html, 'text/html');
+  chrome.runtime.sendMessage(
+    { type: "FETCH_DATA", url: api_url },
+    (response) => {
+      if (response.success) {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(response.data.html, 'text/html');
 
-                // Extract values from the blockquote element
-                const blockquote = doc.querySelector('blockquote');
-                const blueskyUri = blockquote.getAttribute('data-bluesky-uri');
-                const blueskyCid = blockquote.getAttribute('data-bluesky-cid');
-                
-                const didMatch = blueskyUri.match(/did:plc:(\w+)/);
-                const did = didMatch ? didMatch[1] : null;
-                // Extract the post ID from the URI (last segment)
-                const postId = blueskyUri.split('/').pop();
+        // Extract values from the blockquote element
+        const blockquote = doc.querySelector('blockquote');
+        const blueskyUri = blockquote.getAttribute('data-bluesky-uri');
+        const blueskyCid = blockquote.getAttribute('data-bluesky-cid');
 
-                // Log the extracted information
-                console.log('Bluesky URI:', did);
-                console.log('Bluesky CID:', blueskyCid);
-                console.log('Post ID:', postId);
-                saveData([did,postId,blueskyCid])
-            } else {
-                console.error("Error:", response.error);
-            }
-        }
-    );
+        const didMatch = blueskyUri.match(/did:plc:(\w+)/);
+        const did = didMatch ? didMatch[1] : null;
+        // Extract the post ID from the URI (last segment)
+        const postId = blueskyUri.split('/').pop();
+
+        // Log the extracted information
+        console.log('Bluesky URI:', did);
+        console.log('Bluesky CID:', blueskyCid);
+        console.log('Post ID:', postId);
+        saveData([did, postId, blueskyCid])
+      } else {
+        console.error("Error:", response.error);
+      }
+    }
+  );
 }
 
-async function addBookmark(){
-    bookmark_pressed = !bookmark_pressed
-    if (bookmark_pressed){
-        bookmark_div.firstChild.firstChild.setAttribute("d",bookmark_filled)
-        bookmark_div.firstChild.firstChild.setAttribute("fill", bluesky_blue)
-        const api_url = document.head.querySelector('[type="application/json+oembed"]').href;
-        console.log(await getData(api_url))
-    }else{
-        bookmark_div.firstChild.firstChild.setAttribute("d",bookmark_path)
-        bookmark_div.firstChild.firstChild.setAttribute("fill", default_gray)
-    }
+async function addBookmark() {
+  bookmark_pressed = !bookmark_pressed
+  if (bookmark_pressed) {
+    bookmark_div.firstChild.firstChild.setAttribute("d", bookmark_filled)
+    bookmark_div.firstChild.firstChild.setAttribute("fill", bluesky_blue)
+    const api_url = document.head.querySelector('[type="application/json+oembed"]').href;
+    console.log(await getData(api_url))
+  } else {
+    bookmark_div.firstChild.firstChild.setAttribute("d", bookmark_path)
+    bookmark_div.firstChild.firstChild.setAttribute("fill", default_gray)
+  }
 
-    
+
 
 
 }
 
 window.addEventListener('load', function () {
-    const share_button = document.querySelector('[data-testid="shareBtn"]');
-    const share_div = share_button.parentNode;
-    const buttons_container = share_div.parentNode;
-    bookmark_div = share_button.cloneNode(true);
-    bookmark_div.setAttribute("data-testid","bookmarkBtn")
-    share_div.insertAdjacentElement("beforebegin", bookmark_div)
+  const share_button = document.querySelector('[data-testid="shareBtn"]');
+  const share_div = share_button.parentNode;
+  const buttons_container = share_div.parentNode;
+  bookmark_div = share_button.cloneNode(true);
+  bookmark_div.setAttribute("data-testid", "bookmarkBtn")
+  share_div.insertAdjacentElement("beforebegin", bookmark_div)
 
-    chrome.storage.local.get(['bookmarks'], (result) => {
-        var current_url = this.window.location.href;
-        var last_index = current_url.lastIndexOf("/");
-        for(i=result.length-1;i<=0;i--){
-            if (result[i][1] == current_url.substring(last_index + 1)) {
-                // we've already bookmarked this
-                bookmark_div.firstChild.firstChild.setAttribute("d",bookmark_filled)
-                bookmark_div.firstChild.firstChild.setAttribute("fill", bluesky_blue)
-                bookmark_pressed=true
-            };
-        };
-        bookmark_div.firstChild.firstChild.setAttribute("d",bookmark_path)
-    });
+  chrome.storage.local.get(['bookmarks'], (result) => {
+    let bookmarks = result['bookmarks'] || [];
+    const current_url = this.window.location.href;
+    const url_without_query = current_url.split("?")[0];
+    const last_index = url_without_query.lastIndexOf("/");
+    const post_id = url_without_query.substring(last_index + 1);
+    console.log(post_id)
+    console.log(bookmarks.length)
+    for (i = bookmarks.length - 1; i > 0; i--) {
+      console.log(bookmarks[i][1])
+      if (bookmarks[i][1] == post_id) {
+        // we've already bookmarked this
+        bookmark_div.firstChild.firstChild.setAttribute("d", bookmark_filled)
+        bookmark_div.firstChild.firstChild.setAttribute("fill", bluesky_blue)
+        bookmark_pressed = true
+        return
+      };
+    };
+    bookmark_div.firstChild.firstChild.setAttribute("d", bookmark_path)
+  });
 
-    
-
-    bookmark_div.addEventListener("click", addBookmark)
+  bookmark_div.addEventListener("click", addBookmark)
 })
 
