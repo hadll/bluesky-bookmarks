e082d71d2420ab49e47f2cfe6cc971dcc26cbf50
Haddle <94085928+hadll@users.noreply.github.com>
2024-10-24 04:26:50 +1100

    added install popup

--- background.js
+++ background.js
@@ -1,13 +1,17 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.type === 'FETCH_DATA') {
-      fetch(request.url, {
-        method: 'GET',
-        headers: request.headers
-      })
-        .then(response => response.json())
-        .then(data => sendResponse({ success: true, data }))
-        .catch(error => sendResponse({ success: false, error }));
-      return true;
-    }
-  });
-  
+  if (request.type === 'FETCH_DATA') {
+    fetch(request.url, {
+      method: 'GET',
+      headers: request.headers
+    })
+      .then(response => response.json())
+      .then(data => sendResponse({ success: true, data }))
+      .catch(error => sendResponse({ success: false, error }));
+    return true;
+  }
+});
+chrome.runtime.onInstalled.addListener(function (object) {
+  if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+      chrome.tabs.create({ url: chrome.runtime.getURL("install.html")});
+  }
+});
