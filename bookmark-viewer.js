ac121d1b7c1a6167bb950de85c9fe9a9eafeef15
Haddle <94085928+hadll@users.noreply.github.com>
2025-01-12 02:53:02 +1100

    V2

--- bookmark-viewer.js
+++ bookmark-viewer.js
@@ -1,15 +1,165 @@
-chrome.storage.local.get(['bookmarks'], (result) => {
-    console.log("bookmarks gotten")
-    let bookmarks = result['bookmarks'] || [];
-    console.log(bookmarks.length)
-    for(i=bookmarks.length-1;i>=0;i--){
-        console.log(bookmarks[i])
-        const [did, postid, cid] = bookmarks[i]
-        const embed_element = document.createElement("blockquote")
-        embed_element.setAttribute("class", "bluesky-embed")
-        embed_element.setAttribute("data-bluesky-uri", `at://did:plc:${did}/app.bsky.feed.post/${postid}`)
-        embed_element.setAttribute("data-bluesky-cid", cid)
-        this.document.body.insertAdjacentElement("beforeend",embed_element)
+var bookmarks
+
+var page_number = 0
+var page_size = 50
+
+var bookmarks_content
+
+var inputPageSizeElement
+var inputPageNumberElement
+
+// async function get_contents(){
+//     GET https://public.api.bsky.app/xrpc/app.bsky.feed.getPostThread?uri=at://did:plc:6ue4xkpybsoukncszva67yoi/app.bsky.feed.post/3la5eiioyoj27&depth=0&parentHeight=0
+// }
+
+function load_bookmark(bookmark){
+    const [did, postid, cid] = bookmark
+    const embed_element = document.createElement("blockquote")
+    embed_element.setAttribute("class", "bluesky-embed")
+    embed_element.setAttribute("data-bluesky-uri", `at://did:plc:${did}/app.bsky.feed.post/${postid}`)
+    embed_element.setAttribute("data-bluesky-cid", cid)
+    document.getElementById("posts").insertAdjacentElement("beforeend",embed_element)
+}
+
+function load_page(){
+    console.log("--------------------------------------------")
+    const totalBookmarks = bookmarks.length;
+    const startIndex = totalBookmarks - (page_number * page_size) - 1;
+    const endIndex = Math.max(startIndex - page_size + 1, 0);
+    for (let i = startIndex; i >= endIndex; i--) {
+        load_bookmark(bookmarks[i]);
+        console.log(i)
     }
     scan()
+}
+
+function update_page_display(){
+    document.getElementById("current-page-input").value = page_number
+    document.getElementById("page-size-input").value = page_size
+    const nearby_pages = document.getElementById("nearby-pages")
+    const last_page = Math.ceil(bookmarks.length/page_size)-1
+    if (page_number >= 4) {
+        nearby_pages.children[0].innerHTML = "0"
+        nearby_pages.children[1].innerHTML = "..."
+        
+    }else{
+        nearby_pages.children[0].innerHTML = ""
+        nearby_pages.children[1].innerHTML = ""
+        
+    }
+    if (page_number <= last_page-4) {
+        nearby_pages.children[9].innerHTML = last_page.toString()
+        nearby_pages.children[8].innerHTML = "..."
+    }else{
+        nearby_pages.children[9].innerHTML = ""
+        nearby_pages.children[8].innerHTML = ""
+    }
+    for (let i = 1; i<=3; i++) {
+        var previous_page_number = document.getElementById("previous"+i.toString())
+        if (page_number-i >= 0) {
+            previous_page_number.innerHTML = (page_number-i).toString()
+        }else{
+            previous_page_number.innerHTML = ""
+        }
+        var future_page_number = document.getElementById("future"+i.toString())
+        if (page_number+i<=last_page) {
+            future_page_number.innerHTML = (page_number+i).toString()
+        }else{
+            future_page_number.innerHTML = ""
+        }
+    }
+}
+
+function clear_bookmarks(){
+    var posts = document.getElementById("posts")
+    while (posts.firstChild) {
+        posts.removeChild(posts.firstChild);
+    }
+}
+
+function increment_page(difference){
+    if (page_number + difference >= 0 && page_number + difference <= Math.floor((bookmarks.length-1)/page_size)){
+        page_number += difference
+        update_page_display()
+        clear_bookmarks()
+        load_page();
+    }
+}
+
+function change_page(){
+    var new_value = Number(inputPageNumberElement.value)
+    if (new_value != page_number){
+        if (isNaN(new_value)) {
+            return
+        }
+        page_number = Math.min(Math.max(new_value, 0),Math.floor(bookmarks.length/page_size))
+        update_page_display()
+        clear_bookmarks()
+        load_page()
+    }
+}
+
+function change_page_size(new_value){
+    var new_value = Number(inputPageSizeElement.value)
+    if (new_value != page_size){
+        // make sure they actually changed it
+        if (isNaN(new_value) || new_value<0) {
+            return
+        }
+        page_number = Math.floor(Math.min(bookmarks.length - 1, page_size*(page_number + 1) - 1) / new_value)
+        page_size = new_value
+        chrome.storage.local.set({'page_size': page_size},() => {})
+        console.log(bookmarks.length)
+        console.log(page_size)
+        console.log(page_number)
+        console.log(Math.floor(Math.min(bookmarks.length - 1, page_size*(page_number + 1) - 1) / new_value))
+        update_page_display()
+        clear_bookmarks()
+        load_page()
+    }
+}
+
+function click_page_number(number_element){
+    console.log(number_element)
+    inputPageNumberElement.value = Number(number_element.innerHTML)
+    change_page()
+}
+
+chrome.storage.local.get(['bookmarks']).then((result) => {
+    bookmarks = result['bookmarks'] || [];
+    chrome.storage.local.get(['page_size']).then((result) => {
+        if (result['page_size'] != undefined) {
+            page_size = result['page_size']
+        }else{
+            page_size = 50
+            chrome.storage.local.set({'page_size': 50},() => {})
+        }
+        page_size = result['page_size'] || [];
+        update_page_display()
+        load_page()
+    })
 })
+
+document.addEventListener('DOMContentLoaded', function () {
+    inputPageSizeElement = document.getElementById('page-size-input');
+    if (inputPageSizeElement) {
+        inputPageSizeElement.addEventListener('blur', change_page_size);
+        document.getElementById('page-size-input-form').addEventListener('submit', change_page_size);
+    } else {
+        console.error('Element with ID "page-size-input" not found.');
+    }
+    inputPageNumberElement = document.getElementById('current-page-input');
+    if (inputPageNumberElement) {
+        inputPageNumberElement.addEventListener('blur', change_page);
+        document.getElementById('current-page-input-form').addEventListener('submit', change_page_size);
+    }
+    document.getElementById('next-page-button').addEventListener('click', () => increment_page(1))
+    document.getElementById('previous-page-button').addEventListener('click', () => increment_page(-1))
+    const nearby_pages = document.getElementById("nearby-pages")
+    for (var i = 0; i < nearby_pages.children.length; i++){
+        if (!(i == 1 || i == 8)) {
+            let j = nearby_pages.children[i]
+            nearby_pages.children[i].addEventListener('click', (() => click_page_number(j)).bind(this))
+        }
+    }
+});
---
+++ inject-button-firefox.js
@@ -0,0 +1,145 @@
+const bookmark_path = "M4 4.5C4 3.12 5.119 2 6.5 2h11C18.881 2 20 3.12 20 4.5v18.44l-8-5.71-8 5.71V4.5zM6.5 4c-.276 0-.5.22-.5.5v14.56l6-4.29 6 4.29V4.5c0-.28-.224-.5-.5-.5h-11z";
+const bookmark_filled = "M4 4.5C4 3.12 5.119 2 6.5 2h11C18.881 2 20 3.12 20 4.5v18.44l-8-5.71-8 5.71V4.5z";
+
+const bluesky_blue = "hsl(211, 99.1%, 56.1%)";
+const default_gray = "hsl(211, 20%, 56%)";
+
+let bookmark_div;
+let bookmark_pressed = false;
+
+const MAX_STORAGE_BYTES = 10 * 1024 * 1024;
+
+function getSizeInBytes(object) {
+  const jsonStr = JSON.stringify(object);
+  return new Blob([jsonStr]).size;
+}
+
+function waitForElm(selector) {
+  return new Promise(resolve => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
+
+    const observer = new MutationObserver(mutations => {
+      if (document.querySelector(selector)) {
+        observer.disconnect();
+        resolve(document.querySelector(selector));
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true
+    });
+  });
+}
+
+function saveData(newBookmark) {
+  (browser || chrome).storage.local.get(['bookmarks'], (result) => {
+    let updatedArray;
+    let currentArray = result['bookmarks'] || [];
+    
+    if (bookmark_pressed) {
+      updatedArray = [...currentArray, newBookmark];
+    } else {
+      updatedArray = currentArray.filter(bookmark => bookmark[1] !== newBookmark[1]);
+    }
+
+    (browser || chrome).storage.local.set({ 'bookmarks': updatedArray }, () => {
+      console.log("Bookmarks updated!");
+    });
+  });
+}
+
+async function addBookmark() {
+  bookmark_pressed = !bookmark_pressed;
+
+  if (bookmark_pressed) {
+    bookmark_div.firstChild.firstChild.setAttribute("d", bookmark_filled);
+    bookmark_div.firstChild.firstChild.setAttribute("fill", bluesky_blue);
+  } else {
+    bookmark_div.firstChild.firstChild.setAttribute("d", bookmark_path);
+    bookmark_div.firstChild.firstChild.setAttribute("fill", default_gray);
+  }
+
+  const buttons = document.querySelectorAll('[data-testid="bookmarkBtn"]');
+  const button = buttons[Math.min(buttons.length - 1, 6)];
+  const media_container = button.parentNode.parentNode.parentNode.firstChild;
+  const images = media_container.getElementsByTagName("img");
+  const image = images[0];
+  const context_container = media_container.parentNode.parentNode.firstChild;
+
+  let pfp_src = context_container.getElementsByTagName("img")[0].src.split("/");
+  let did = pfp_src[pfp_src.length - 2].replace("did:plc:", "");
+  let cid;
+
+  if (image !== undefined) {
+    let image_src = image.src.split("/");
+    cid = image_src[7].replace("@jpeg", "");
+  } else {
+    const video = media_container.getElementsByTagName("video")[0];
+    if (video !== undefined) {
+      let video_src = video.getAttribute("poster").split("/");
+      cid = video_src[video_src.length - 2];
+    }
+  }
+
+  const url = window.location.href;
+  const pid = url.split("/").pop().split("?")[0];
+
+  saveData([did, pid, cid]);
+}
+
+async function addButton(event) {
+  bookmark_pressed = false;
+  const current_url = window.location.href;
+  const spliturl = current_url.split("/");
+
+  if (spliturl[spliturl.length - 2] !== "post") return;
+
+  const to_be_gone = document.querySelectorAll('[data-testid="shareBtn"]');
+  if (to_be_gone.length > 1) to_be_gone[0].parentNode.remove();
+
+  const share_button = await waitForElm('[data-testid="shareBtn"]');
+  const share_div = share_button.parentNode;
+  bookmark_div = share_button.cloneNode(true);
+  bookmark_div.setAttribute("data-testid", "bookmarkBtn");
+  bookmark_div.setAttribute("aria-label", "Bookmark");
+  share_div.insertAdjacentElement("beforebegin", bookmark_div);
+
+  const all_bookmark_buttons = share_div.parentNode.querySelectorAll('[data-testid="bookmarkBtn"]');
+  for (let i = 0; i < all_bookmark_buttons.length - 1; i++) {
+    all_bookmark_buttons[i].remove();
+  }
+
+  (browser || chrome).storage.local.get(['bookmarks'], (result) => {
+    let bookmarks = result['bookmarks'] || [];
+    const url_without_query = current_url.split("?")[0];
+    const last_index = url_without_query.lastIndexOf("/");
+    const post_id = url_without_query.substring(last_index + 1);
+
+    for (let i = bookmarks.length - 1; i >= 0; i--) {
+      if (bookmarks[i][1] === post_id) {
+        bookmark_div.firstChild.firstChild.setAttribute("d", bookmark_filled);
+        bookmark_div.firstChild.firstChild.setAttribute("fill", bluesky_blue);
+        bookmark_pressed = true;
+        return;
+      }
+    }
+
+    bookmark_div.firstChild.firstChild.setAttribute("d", bookmark_path);
+  });
+
+  bookmark_div.addEventListener("click", addBookmark);
+}
+
+// Adding navigation check for Firefox
+let current_url = window.location.href;
+setInterval(() => {
+  if (window.location.href !== current_url) {
+    current_url = window.location.href;
+    addButton();
+  }
+}, 500);
+
+addButton();
