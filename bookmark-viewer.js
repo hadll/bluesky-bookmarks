0e7739127ecd776abe98642e26e61ace54b6a7e0
Haddle <94085928+hadll@users.noreply.github.com>
2025-03-12 15:18:19 +1100

    wip commit
    i should probably commit more in progress stuff so here ya go

--- bookmark-viewer.js
+++ bookmark-viewer.js
@@ -1,4 +1,5 @@
 var bookmarks
+var tags
 
 var page_number = 0
 var page_size = 50
@@ -12,13 +13,77 @@ var inputPageNumberElement
 //     GET https://public.api.bsky.app/xrpc/app.bsky.feed.getPostThread?uri=at://did:plc:6ue4xkpybsoukncszva67yoi/app.bsky.feed.post/3la5eiioyoj27&depth=0&parentHeight=0
 // }
 
+function save_tags(){
+    chrome.storage.local.set({ 'tags': tags },()=> {})
+}
+
+function tag_context_button_click(tag, is_post){
+    if (is_post){
+        tagged_posts = tags[tag.getElementById("name").innerText]["tags"]
+        tagged_posts.splice(tagged_posts.indexOf(tag.id),1)
+        save_tags()
+    }
+    tag.remove()
+}
+
+function create_new_tag(name, colour){
+    tags[name] = []
+    tags[name]["color"] = colour
+    tags[name]["tags"] = []
+}
+
+function create_tag_element(parent, name, colour, is_post){
+    const tag_element = document.createElement("div")
+    tag_element.setAttribute("class", "tag")
+    tag_element.style["background-color"] = colour
+    const context_button = document.createElement("img")
+    context_button.setAttribute("class","tag-button")
+    const tag_name = document.createElement("span")
+    tag_name.id = "name"
+    tag_name.innerText = name
+    tag_element.appendChild(context_button)
+    tag_element.appendChild(tag_name)
+    parent.appendChild(tag_element)
+
+    context_button.addEventListener('click', () => tag_context_button_click(tag_element, is_post))
+}
+
+function create_add_tag(parent, is_post){
+    const tag_element = document.createElement("div")
+    tag_element.setAttribute("class", "tag")
+    const context_button = document.createElement("img")
+    context_button.setAttribute("class","add-tag-button")
+    const tag_name = document.createElement("span")
+    tag_name.id = "name"
+    tag_name.innerText = "Add Tag"
+    tag_element.appendChild(context_button)
+    tag_element.appendChild(tag_name)
+    parent.appendChild(tag_element)
+
+    context_button.addEventListener('click', () => tag_context_button_click(tag_element, is_post))
+}
+
 function load_bookmark(bookmark){
     const [did, postid, cid] = bookmark
     const embed_element = document.createElement("blockquote")
     embed_element.setAttribute("class", "bluesky-embed")
     embed_element.setAttribute("data-bluesky-uri", `at://did:plc:${did}/app.bsky.feed.post/${postid}`)
     embed_element.setAttribute("data-bluesky-cid", cid)
+    embed_element.id = postid
+
     document.getElementById("posts").insertAdjacentElement("beforeend",embed_element)
+
+    const tag_container = document.createElement("div")
+    tag_container.setAttribute("class", "tag-container")
+    document.getElementById("posts").insertAdjacentElement("beforeend", tag_container)
+
+    for (i=0; i<=0; i++){
+        create_tag_element(tag_container, "Meme", "rgb(83, 112, 255)")
+        create_tag_element(tag_container, "Video Games", "rgb(83, 255, 109)")
+        create_tag_element(tag_container, "Image", "rgb(195, 83, 255)")
+        create_add_tag(tag_container, false)
+    }
+    console.log(tag_container.parentElement)
 }
 
 function load_page(){
@@ -135,10 +200,15 @@ chrome.storage.local.get(['bookmarks']).then((result) => {
             chrome.storage.local.set({'page_size': 50},() => {})
         }
         page_size = result['page_size'] || [];
+        chrome.storage.local.get(['tags']).then((result) => {
+            tags = result['tags'] || [];
+        })
         update_page_display()
         load_page()
     })
 })
+
+
 
 document.addEventListener('DOMContentLoaded', function () {
     inputPageSizeElement = document.getElementById('page-size-input');
